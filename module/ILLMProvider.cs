c657bcc9e10433c506b6968e5f19e79e0275e790
yh.kim <yh.kim@ncodi.co.kr>
2026-06-12 10:34:09 +0900

    ILLMProvider.cs에 HasQuota, HasBalance 속성 추가

--- module/ILLMProvider.cs
+++ module/ILLMProvider.cs
@@ -4,6 +4,9 @@ public interface ILlmProvider {
     Task<Quota> GetCurrentQuotaAsync();
     Task<Balance> GetCurrentBalanceAsync();
     
+    public abstract bool HasQuota { get; }
+    public abstract bool HasBalance { get; }
+    
     public abstract class Quota {
         public double Daily { get; set; }
         public double Weekly { get; set; }
