7b887adf4d6fd3030b22af60d715cdf6574bb169
yh.kim <yh.kim@ncodi.co.kr>
2026-06-12 10:29:31 +0900

    ILLMProvider.cs 추가

---
+++ module/ILLMProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace LLMUsageBar.module;
+
+public interface ILlmProvider {
+    Task<Quota> GetCurrentQuotaAsync();
+    Task<Balance> GetCurrentBalanceAsync();
+    
+    public abstract class Quota {
+        public double Daily { get; set; }
+        public double Weekly { get; set; }
+    }
+    
+    public abstract class Balance {
+        public double Remain { get; set; }
+    }
+}
+
+
