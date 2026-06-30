a6c26cc74d1aff5df92aaf23a47b84de0a50f8c2
yh.kim <yh.kim@ncodi.co.kr>
2026-06-30 17:11:52 +0900

    최댓값이 있는 경우 프로그레스바 형태로 표시

--- Util/NonZeroToVisibilityConverter.cs
+++ Util/NonZeroToVisibilityConverter.cs
@@ -6,13 +6,19 @@ namespace LLMUsageBar.Util;
 
 /// <summary>
 /// double 값이 0이 아니면 Visible, 0이면 Collapsed로 변환합니다.
+/// Invert 파라미터를 전달하면 결과를 반대로 변환합니다.
 /// </summary>
 public class NonZeroToVisibilityConverter : IValueConverter {
     /// <summary>
-    /// 값이 0이 아닌 경우 Visible, 0인 경우 Collapsed로 변환합니다.
+    /// 값이 0이 아닌 경우 Visible, 0인 경우 Collapsed로 변환하고, Invert 파라미터가 있으면 결과를 반대로 바꿉니다.
     /// </summary>
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-        if (value is double d && d > 0) {
+        bool isVisible = value is double d && d > 0;
+        if (parameter is string stringParameter && stringParameter.Equals("Invert", StringComparison.OrdinalIgnoreCase)) {
+            isVisible = !isVisible;
+        }
+
+        if (isVisible) {
             return Visibility.Visible;
         }
         return Visibility.Collapsed;
